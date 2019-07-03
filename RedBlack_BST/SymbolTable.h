2f08559ba33a5c786074a49daef04b08d8a4fcd5
Matthew <matkit27@gmail.com>
2019-07-03 16:33:20 +0300

    Red-Black BST without Deletion (not tested)

--- RedBlack_BST/SymbolTable.h
+++ RedBlack_BST/SymbolTable.h
@@ -10,6 +10,8 @@ template<typename T, typename KeyT = std::string>
 class SymbolTable
 {
 private:
+	//-----Nested helper structures-----
+
 	struct KeyIterator
 	{
 	public:
@@ -54,10 +56,13 @@ private:
 			return 1;
 		}
 	};
+	
+	//-----Variables-----
 
 	Compare comparator;
-
 	NodePointer<T, KeyT> Root;
+
+	//-----Minimum and Maximum-----
 
 	NodePointer<T, KeyT> DeleteMin(NodePointer<T, KeyT> node)
 	{
@@ -69,7 +74,7 @@ private:
 
 		return node;
 	}
-
+	
 	NodePointer<T, KeyT>& GetMin(NodePointer<T, KeyT>& node)
 	{
 		while (node->Left != nullptr)
@@ -89,6 +94,8 @@ private:
 		return node;
 	}
 
+	//-----Iteration and Size-----
+
 	void Inorder(NodePointer<T, KeyT> node, std::vector<KeyT>& array)
 	{
 		if (node == nullptr)
@@ -107,6 +114,51 @@ private:
 		return node->Size;
 	}
 
+	//-----Red-Black balancing helper functions----
+
+	bool IsRed(NodePointer<T, KeyT>& node)
+	{
+		if (node == nullptr)
+			return false;
+
+		return node->Color == RefColor::Red;
+	}
+
+	bool Split(NodePointer<T, KeyT>& node)
+	{
+		if (node->Left->Color == RefColor::Red && node->Right->Color == RefColor::Red)
+		{
+			node->Right->Color = RefColor::Black;
+			node->Left->Color = RefColor::Black;
+			node->Color = RefColor::Red;
+			return true;
+		}
+		return false;
+	}
+
+	NodePointer<T, KeyT>& RotateLeft(NodePointer<T, KeyT>& node)
+	{
+		NodePointer<T, KeyT> temp = node->Left;
+		temp->Color = node->Color;
+		node->Color = RefColor::Red;
+		node->Left = temp->Right;
+		temp->Left = node;
+
+		return temp;
+	}
+	NodePointer<T, KeyT>& RotateRight(NodePointer<T, KeyT>& node)
+	{
+		NodePointer<T, KeyT> temp = node->Right;
+		temp->Color = node->Color;
+		node->Color = RefColor::Red;
+		node->Right = temp->Left;
+		temp->Left = node;
+
+		return temp;
+	}
+
+	//-----Deletion and Insertion-----
+
 	NodePointer<T, KeyT>& Insert(NodePointer<T, KeyT>& node, NodePointer<T, KeyT>& new_node)
 	{
 		if (node == nullptr)
@@ -120,6 +172,13 @@ private:
 			node->Right = Insert(node->Right, new_node);
 		else
 			node->Value = new_node->Value;
+
+		if (IsRed(node->Right) && !IsRed(node->Left))
+			RotateLeft(node);
+		if (IsRed(node->Left) && IsRed(node->Left->Left))
+			RotateRight(node);
+		if (IsRed(node->Left) && IsRed(node->Right))
+			Split(node);
 
 		node->Size = 1 + Size(node->Left) + Size(node->Right);
 
